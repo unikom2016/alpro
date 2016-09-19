7bf1c46a10fabe199333029c78f0658b0fb132f0
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-09-19 15:03:10 +0700

    [FIX] Description or label
    -give a brief description or label

--- 001_alpro/002_exchange_values/exchange_values.pas
+++ 001_alpro/002_exchange_values/exchange_values.pas
@@ -5,6 +5,9 @@ var angka1, angka2 : integer;
 begin
   write('Masukkan angka1: '); readln(angka1);
   write('Masukkan angka2: '); readln(angka2);
+  writeln('Angka1 saat ini: ', angka1);
+  writeln('Angka2 saat ini: ', angka2);
+  writeln();
   angka1 := angka1 + angka2;
   angka2 := angka1 - angka2;
   angka1 := angka1 - angka2;
