76c61d794cdcae80915ba06ebd9010bef19bebce
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-09-19 14:42:34 +0700

    [ADD] Exchange between two values without using Third variables
    # a = a + b
    # b = a - b
    # a = a - b

---
+++ 001_alpro/002_exchange_values/exchange_values.pas
@@ -0,0 +1,14 @@
+program exchange_values;
+
+var angka1, angka2 : integer;
+
+begin
+  write('Masukkan angka1: '); readln(angka1);
+  write('Masukkan angka2: '); readln(angka2);
+  angka1 := angka1 + angka2;
+  angka2 := angka1 - angka2;
+  angka1 := angka1 - angka2;
+  writeln('Keadaan dua buah angka setelah bertukar!');
+  writeln('Angka1 saat ini: ', angka1);
+  writeln('Angka2 saat ini: ', angka2);
+end.
