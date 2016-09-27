9a1bdbfa5c7f0016671a4697bb690b2aa9f80481
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2016-09-28 05:13:25 +0700

    [DONE] Parking Fee
    -New algorithms

--- 004_homework/001_fee/Fee.pas
+++ 004_homework/001_fee/Fee.pas
@@ -1,46 +1,56 @@
+{ Judul }
 Program Fee;
 
-{I.S. : }
-{F.S. : }
+{I.S. : User memasukkan data Nomor Polisi, Jam & Menit Mulai, Jam & Menit Selesai}
 
-// Kamus
-var NomorPolisi, 
-	JamMasuk, MenitMasuk,
-	JamSelesai, MenitSelesai,
-	SelisihJam, SelisihMenit,
-	BiayaAwalParkir, TotalBiaya,
-	KonversiJamAwal, KonversiJamAkhir,
+{F.S. : Menampilkan total biaya parkir}
+
+{ Kamus }
+	// Nomor Polisi
+var NomorPolisi : string;
+	// Jam Mulai & Selesai
+var JamMulai, JamSelesai, Menit,  
+	// Durasi & Konversi
+	DurasiMenit, KonversiJamMulai, KonversiJamSelesai,
+	// Selisih
+	SelisihJam,  
+	// Biaya
+	BiayaAwalParkir, TotalBiaya, 
+	// Const
 	JAM_PER_MENIT : integer;
 
-// Algoritma
+{ Algoritma }
 begin
-
 	JAM_PER_MENIT := 60;
 	BiayaAwalParkir := 1500;
 	TotalBiaya := BiayaAwalParkir;
 
-	// Input
+	{ Masukkan }
 	write('Masukkan Nomor Polisi : '); readln(NomorPolisi);
+	// Jam mulai, menggunakan Format 24 Jam
+	write('Masukkan Jam Mulai (24h) : '); readln(JamMulai);
+	write('Masukkan Menit Mulai : '); readln(Menit);
+	{ Konversi Jam Mulai }
+	KonversiJamMulai := (JamMulai * JAM_PER_MENIT) + Menit; // Mengubah Jam Masuk ke Menit
 
-	write('Masukkan Jam Masuk : '); readln(JamMasuk);
-	write('Masukkan Menit Masuk : '); readln(MenitMasuk);
+	// Jam selesai, menggunakan Format 24 Jam
+	write('Masukkan Jam Selesai (24h) : '); readln(JamSelesai);
+	write('Masukkan Menit Selesai : '); readln(Menit);
+	{ Konversi Jam Selesai }
+	KonversiJamSelesai := (JamSelesai * JAM_PER_MENIT) + Menit; // Mengubah Jam Selesai ke Menit
 	
-	write('Masukkan Jam Selesai : '); readln(JamSelesai);
-	write('Masukkan Menit Selesai : '); readln(MenitSelesai);
+	{ Proses }
+	DurasiMenit := KonversiJamSelesai - KonversiJamMulai;
 
-	// Process
-	KonversiJamAwal := (JamMasuk * JAM_PER_MENIT) + MenitMasuk;
-	KonversiJamAkhir := (JamSelesai * JAM_PER_MENIT) + MenitSelesai;
-	SelisihMenit := KonversiJamAkhir - KonversiJamAwal;
-	writeln('Selisih Menit : ', SelisihMenit);
+	{ Kondisi (Pemilihan) }
+	If (DurasiMenit >= JAM_PER_MENIT)
+		Then
+			SelisihJam := JamSelesai - JamMulai;
+			TotalBiaya := BiayaAwalParkir + (500 * SelisihJam);
 
-	// Kondisi
-	If (SelisihMenit > JAM_PER_MENIT) Then
-			SelisihJam := JamSelesai - JamMasuk; // Format 24 Hour
-	    	TotalBiaya := BiayaAwalParkir + (500 * SelisihJam);
-
-	// Output
-	writeln('Dengan Nomor Polisi : ', NomorPolisi);
-	writeln('Total Biaya yang perlu di bayarkan, yaitu Rp : ', TotalBiaya);
-
+	{ Output }
+	writeln();
+	writeln('Lama parkir (menit) : ', DurasiMenit, 'm');
+	writeln('Nomor Polisi : ', NomorPolisi);
+	writeln('Membayar parkir sejumlah : Rp', TotalBiaya);
 end.
--- 004_homework/001_fee/README.md
+++ 004_homework/001_fee/README.md
@@ -1,42 +1,46 @@
-// Judul
-Menentukan_biaya_parkir
+    { Judul }
+    Menentukan_biaya_parkir
 
-{I.S. : User memasukkan data Nomor Polisi, Jam & Menit Masuk, Jam & Menit Selesai}
-{F.S. : Menampilkan total biaya parkir}
+    {I.S. : User memasukkan data Nomor Polisi, Jam & Menit Masuk, Jam & Menit Selesai}
+    
+	{F.S. : Menampilkan total biaya parkir}
 
-// Kamus
-	// Nomor Polisi
-	NomorPolisi : string
-	// Jam Masuk
-	JamMasuk, MenitMasuk : integer
-	// Jam Keluar
-	JamKeluar, MenitKeluar : integer
-	// Selisih & Konversi
-	SelisihMenit, KonversiJamMasuk, KonversiJamKeluar : integer
-	// Biaya
-	BiayaParkir, TotalBiaya : integer
-	// Const
-	JAM_PER_MENIT : integer
+	{ Kamus }
+		// Nomor Polisi
+		NomorPolisi : string
+		// Jam Masuk
+		JamMasuk, MenitMasuk, 
+		// Jam Keluar
+		JamKeluar, MenitKeluar, 
+		// Durasi & Konversi
+		DurasiMenit, KonversiJamMasuk, KonversiJamKeluar, 
+		// Biaya
+		BiayaParkir, TotalBiaya, 
+		// Const
+		JAM_PER_MENIT : integer
 
-// Algoritma
-	JAM_PER_MENIT <- 60
-	BiayaParkir <- 1500
-	TotalBiaya <- BiayaParkir
+    { Algoritma }
+		JAM_PER_MENIT <- 60
+		BiayaParkir <- 1500
+		TotalBiaya <- BiayaParkir
 
-	// Input
-	Input(NomorPolisi, JamMasuk, MenitMasuk, JamKeluar, MenitKeluar) // Format 24 Jam
-	
-	// Proses
-	KonversiJamMasuk <- (JamMasuk * JAM_PER_MENIT) + MenitMasuk
-	KonversiJamKeluar <- (JamKeluar * JAM_PER_MENIT) + MenitKeluar
-	DurasiMenit <- KonversiJamKeluar - KonversiJamMasuk
+		{ Masukkan }
+		Input(NomorPolisi, Jam, Menit) // Jam masuk, menggunakan Format 24 Jam
+		KonversiJamMasuk <- (Jam * JAM_PER_MENIT) + Menit // Mengubah Jam Masuk ke Menit
 
-	// Kondisi (Pemilihan)
-	If (DurasiMenit > JAM_PER_MENIT)
-	    Then
-			SelisihJam <- JamKeluar - JamMasuk
-			TotalBiaya <- BiayaParkir + (500 * SelisihJam)
-	EndIf
+		Input(Jam, Menit) // Jam keluar, menggunakan Format 24 Jam
+		KonversiJamKeluar <- (Jam * JAM_PER_MENIT) + Menit // Mengubah Jam Keluar ke Menit
+		
+		{ Proses }
+		DurasiMenit <- KonversiJamKeluar - KonversiJamMasuk
 
-	// Output
-	Output(TotalBiaya)
+		{ Kondisi (Pemilihan) }
+		If (DurasiMenit > JAM_PER_MENIT)
+			Then
+				SelisihJam <- JamKeluar - JamMasuk
+				TotalBiaya <- BiayaParkir + (500 * SelisihJam)
+		EndIf
+
+		{ Output }
+		Output('Nomor Polisi : ', NomorPolisi,
+			' Membayar parkir sejumlah : Rp', TotalBiaya);
