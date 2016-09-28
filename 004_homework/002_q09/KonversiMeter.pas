6827af35077d8b9f2d5adc1ce5659d256ad256a7
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2016-09-28 08:04:48 +0700

    [DONE] Bab. 5, Hal. 110, No. 9
    -Why using result:4:2?
    -I leaves it, as-is

--- 004_homework/002_q09/KonversiMeter.pas
+++ 004_homework/002_q09/KonversiMeter.pas
@@ -14,15 +14,16 @@ begin
     Write('Masukkan Panjang Satuan Benda (meter) : '); Readln(PanjangSatuanMeter);
 
     { Proses } 
-    Mm := PanjangSatuanMeter * 1000;
-    Cm := PanjangSatuanMeter * 100;
-    Inchi := Mm / 25.4;
-    Kaki := Cm / 30.48;
-    Yard := PanjangSatuanMeter / 0.9144;
+    Mm := PanjangSatuanMeter * 1000; // Meter ke milimeter
+    Cm := PanjangSatuanMeter * 100; // Meter ke centimeter 
+    Inchi := Mm / 25.4; // Milimeter ke Inchi
+    Kaki := Cm / 30.48; // Centimeter ke Kaki
+    Yard := PanjangSatuanMeter / 0.9144; // Meter ke Yard
     
     { Keluaran }
-    Writeln('Inchi : ', Inchi:4:2);
-    Writeln('Kaki : ', Kaki:4:2);
-    Writeln('Yard : ', Yard:4:2);
+    Writeln('Hasil setelah satuan (meter) dikonversi: ');
+    Writeln('Inchi : ', Inchi);
+    Writeln('Kaki : ', Kaki);
+    Writeln('Yard : ', Yard);
 
 end.
