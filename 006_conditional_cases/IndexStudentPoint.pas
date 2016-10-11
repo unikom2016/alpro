af06dcc4e5d0bd30b7684227648637b8f1efa64c
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2016-10-12 04:44:15 +0700

    [FIX+ADD] Login for 006
    -Login screen, and its throwback when wrong username / password submitted
    -Fix minor bug (Using repeat loop)
    -Indentation

--- 006_conditional_cases/IndexStudentPoint.pas
+++ 006_conditional_cases/IndexStudentPoint.pas
@@ -12,113 +12,123 @@ Const
     User = 'Dwi';
     Pass = '1234';
 
-Var Nama, KetMTK, KetBInd, KetIPA, KetBIng, Keterangan, 
-    NamaPengguna, KataKunci : string;
-Var N_MTK, N_BInd, N_IPA, N_BIng, SK : real;
-Var JumlahRemedial, UrutanMTK, UrutanBInd, UrutanIPA, UrutanBIng : integer;
+Var Nama, Ket, NamaPengguna, KataKunci: string;
+Var N_MTK, N_BInd, N_IPA, N_BIng, SK: real;
+Var BatasLogin, Urutan, i: integer;
 
 { Algoritma }
 Begin
 
     { Input }
-    // LOGIN
-    Window(1, 1, 80, 24); TextBackground(15); ClrScr();
-    GotoXY(32, 10); Write('<< L O G I N >>');
-    GotoXY(32, 12); Write('Nama Pengguna    :'); Readln(NamaPengguna);
-    GotoXY(32, 13); Write('Kata Kunci       :'); Readln(KataKunci);
+    i := 1;
+    BatasLogin := 3;
+    repeat
+        // LOGIN
+        Window(1, 1, 80, 24); TextBackground(0); ClrScr();
+        GotoXY(33, 10); Write('<< L O G I N >>');
+        GotoXY(30, 12); Write('Nama Pengguna    :'); Readln(NamaPengguna);
+        GotoXY(30, 13); Write('Kata Kunci       :'); Readln(KataKunci);
 
-    If ((NamaPengguna <> User) or (KataKunci <> Pass))
-        then
-        begin
-          GotoXY(30, 14);
-          Write('Login gagal!');
-        end
-    Else
-        begin
-        ClrScr();
-        // Data Masukkan
-        // Window(1,1,40,24); TextBackground(15); ClrScr();
-        // GotoXY(20,1); Write('Data Masukkan');
-        // GotoXY(20,2); Write('=============');
-
-        // Data Siswa dan Nilai Mata Pelajaran 
-        GotoXY(1,5);
-        Write('Nama                     :'); Readln(Nama);
-        Write('Nilai Matematika         :'); Readln(N_MTK);
-        Write('Nilai Bahasa Indonesia   :'); Readln(N_BInd);
-        Write('Nilai IPA                :'); Readln(N_IPA);
-        Write('Nilai Bahasa Inggris     :'); Readln(N_BIng);
-        Write('Standar Kelulusan        :'); Readln(SK);
-
-        { Proses }
-        JumlahRemedial := 0;
-
-        { Kondisi (Pemilihan) }
-        // Window(40,1,80,24); TextBackground(15); ClrScr();
-        // GotoXY(20,1); Write('Data Keluaran');
-        // GotoXY(20,2); Write('=============');
-
-        // GotoXY(21,5);
-        // Kondisi n - 2 (Tidak lulus semua matpel)
-        If ((N_MTK < SK) and (N_BInd < SK) and (N_IPA < SK) and (N_BIng < SK))
+        If ((NamaPengguna <> User) Or (KataKunci <> Pass))
             Then
                 Begin
-                    Keterangan := 'maaf kamu harus mengulang!';
-                    Writeln(Nama, ', ', Keterangan);
+                    If (i = BatasLogin) Then
+                        Begin
+                            Ket := 'Maaf kamu gagal login lebih dari 3!';
+                            ClrScr();
+
+                            GotoXY(40 - (length(Ket) div 2), 1);
+                            Writeln(Ket);
+                            Break;
+                        End
+                    Else
+                        Begin
+                            Ket :=  'Username atau password salah!';
+                            GotoXY(40 - (length(Ket) div 2), 14);
+                            Writeln(Ket);
+                            Readln();
+                            i := i + 1;
+                        End;
+                    ClrScr();
                 End
-        // Kondisi ke 'n - 1' (Lulus semua matpel)
-        Else If ((N_MTK >= SK) and (N_BInd >= SK) and (N_IPA >= SK) and (N_BIng >= SK))
-            Then
-                Begin
-                    Keterangan := 'selamat kamu lulus!';
-                    Writeln(Nama, ', ', Keterangan);
-                End
-        // Kondisi ke 'n'
         Else
             Begin
-                Keterangan := 'kamu harus mengikuti ujian ulang mata pelajaran: ';
-                Writeln(Nama, ', ', Keterangan);
+                ClrScr();
+                // Data Masukkan
+                // Window(1,1,40,24); TextBackground(15); ClrScr();
+                // GotoXY(20,1); Write('Data Masukkan');
+                // GotoXY(20,2); Write('=============');
 
-                // Cek Nilai Matematika
-                If (N_MTK < SK) 
-                    Then
-                    Begin
-                        JumlahRemedial += 1;
-                        UrutanMTK := JumlahRemedial;
-                        KetMTK := IntToStr(UrutanMTK) + '. ' + 'Matematika';
-                        Writeln(KetMTK);
-                    End;
-                // Cek Nilai Bahasa Indonesia
-                If (N_BInd < SK)
-                    Then
-                    Begin
-                        JumlahRemedial += 1;
-                        UrutanBInd := JumlahRemedial;
-                        KetBInd := IntToStr(UrutanBInd) + '. ' + 'Bahasa Indonesia';
-                        Writeln(KetBInd);
-                    End;
-                // Cek Nilai IPA
-                If (N_IPA < SK)
-                    Then
-                    Begin
-                        JumlahRemedial += 1;
-                        UrutanIPA := JumlahRemedial;
-                        KetIPA := IntToStr(UrutanIPA) + '. ' + 'IPA';
-                        Writeln(KetIPA);
-                    End;
-                // Cek Nilai Bahasa Inggris
-                If (N_BIng < SK)
-                    Then
-                    Begin
-                        JumlahRemedial += 1;
-                        UrutanBIng := JumlahRemedial;
-                        KetBIng := IntToStr(UrutanBIng) + '. ' + 'Bahasa Inggris';
-                        Writeln(KetBIng);
-                    End;
-            End;
+                // Data Siswa dan Nilai Mata Pelajaran 
+                GotoXY(1,5);
+                Write('Nama                     :'); Readln(Nama);
+                Write('Nilai Matematika         :'); Readln(N_MTK);
+                Write('Nilai Bahasa Indonesia   :'); Readln(N_BInd);
+                Write('Nilai IPA                :'); Readln(N_IPA);
+                Write('Nilai Bahasa Inggris     :'); Readln(N_BIng);
+                Write('Standar Kelulusan        :'); Readln(SK);
 
-    End;
-    
+                { Proses }
+
+                { Kondisi (Pemilihan) }
+                // Window(40,1,80,24); TextBackground(15); ClrScr();
+                // GotoXY(20,1); Write('Data Keluaran');
+                // GotoXY(20,2); Write('=============');
+
+                // GotoXY(21,5);
+
+                // Kondisi n - 2 (Tidak lulus semua matpel)
+                If ((N_MTK < SK) and (N_BInd < SK) and (N_IPA < SK) and (N_BIng < SK))
+                    Then
+                        Begin
+                            Writeln(Nama, ', ', 'maaf kamu harus mengulang!');
+                        End
+                // Kondisi ke 'n - 1' (Lulus semua matpel)
+                Else If ((N_MTK >= SK) and (N_BInd >= SK) and (N_IPA >= SK) and (N_BIng >= SK))
+                    Then
+                        Begin
+                            Writeln(Nama, ', ', 'selamat kamu lulus!');
+                        End
+                // Kondisi ke 'n'
+                Else
+                    Begin
+                        Writeln(Nama, ', ', 'kamu harus mengikuti ujian ulang mata pelajaran: ');
+                        Urutan := 0;
+
+                        // Cek Nilai Matematika
+                        If (N_MTK < SK) 
+                            Then
+                            Begin
+                                Urutan += 1;
+                                Writeln(Urutan, '. Matematika');
+                            End;
+                        // Cek Nilai Bahasa Indonesia
+                        If (N_BInd < SK)
+                            Then
+                            Begin
+                                Urutan += 1;
+                                Writeln(Urutan, '. Bahasa Indonesia');
+                            End;
+                        // Cek Nilai IPA
+                        If (N_IPA < SK)
+                            Then
+                            Begin
+                                Urutan += 1;
+                                Writeln(Urutan, '. IPA');
+                            End;
+                        // Cek Nilai Bahasa Inggris
+                        If (N_BIng < SK)
+                            Then
+                            Begin
+                                Urutan += 1;
+                                Writeln(Urutan, '. Bahasa Inggris');
+                            End;
+                    End;
+
+                    Break;
+                End;
+    Until (i > BatasLogin);
+
     Readln();
 
 End.
