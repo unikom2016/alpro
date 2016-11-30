8556894f0480dd9cc9ff366f25a7d22547b1ee66
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2016-11-30 11:42:51 +0700

    [HOMEWORK - Wk10] - Fix validation on college students

--- 016_excel_wannabe/student_grade.pas
+++ 016_excel_wannabe/student_grade.pas
@@ -3,7 +3,7 @@ program student_grade;
 uses crt;
 
 const
-  max = 2;
+  max = 50;
 
 type
   arr_str = array[1..max] of string;
@@ -30,37 +30,33 @@ end;
 procedure tampil_data;
 var i: integer;
 begin
-  // readln(jml_mhsiswa);
-  // if (jml_mhsiswa <= max) then begin
-    
-  // end;
-  window(2, 2, 79, 29);
+  write('Masukkan banyaknya mahasiswa: '); readln(jml_mhsiswa);
   clrscr;
-  gotoxy(0, 0);
-  writeln('masukkan nim, nama, nilai, indeks (akan terhitung otomatis)');
-  writeln('===========================================================================');
-  writeln('| No |      NIM      |      Nama      |      Nilai      |      Indeks     |');
-  writeln('---------------------------------------------------------------------------');
-   
-  for i := 0 to (max - 1) do begin
-    // gotoxy(x, y)
-    write('|'); gotoxy(4, i + 5); write(i + 1, ' | ');
-    gotoxy(22, i + 5); write('|');
-    gotoxy(39, i + 5); write('|');
-    gotoxy(57, i + 5); write('|');
-    gotoxy(75, i + 5); write('|');
+  if (jml_mhsiswa < max) then begin
+    window(2, 2, 79, 29);
+    clrscr;
+    gotoxy(0, 0);
+    writeln('masukkan nim, nama, nilai, indeks (akan terhitung otomatis)');
+    writeln('===========================================================================');
+    writeln('| No |      NIM      |      Nama      |      Nilai      |      Indeks     |');
+    writeln('---------------------------------------------------------------------------');
+    
+    for i := 0 to (jml_mhsiswa - 1) do begin
+      // gotoxy(x, y)
+      write('|'); gotoxy(4, i + 5); write(i + 1, ' | ');
+      gotoxy(22, i + 5); write('|');
+      gotoxy(39, i + 5); write('|');
+      gotoxy(57, i + 5); write('|');
+      gotoxy(75, i + 5); write('|');
 
-    gotoxy(11, i + 5); readln(nim[i]);
-    gotoxy(28, i + 5); readln(nama[i]);
-    gotoxy(45, i + 5); readln(nilai[i]);
-    gotoxy(66, i + 5);
-    indeks[i] := indeks_nilai(nilai[i]);
-    writeln(indeks[i]);
+      gotoxy(11, i + 5); readln(nim[i]);
+      gotoxy(28, i + 5); readln(nama[i]);
+      gotoxy(45, i + 5); readln(nilai[i]);
+      gotoxy(66, i + 5);
+      indeks[i] := indeks_nilai(nilai[i]);
+      writeln(indeks[i]);
+    end;
   end;
-
-  // for i := 0 to (max - 1) do begin
-  //   writeln( ' | ', nim[i], ' | ', nama[i], ' | ', nilai[i], ' | ', indeks[i]);
-  // end;
 end;
 
 begin
