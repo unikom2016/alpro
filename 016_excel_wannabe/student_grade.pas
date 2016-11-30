53f2b0fe3757c486524f00ade7ce61b20efac38f
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2016-11-30 17:23:00 +0700

    [HOMEWORK - Wk01] - Find minimum and maximum value function #done - Fix table layouting - Refactor indeks_nilai to idx, for more clean code

--- 016_excel_wannabe/student_grade.pas
+++ 016_excel_wannabe/student_grade.pas
@@ -6,15 +6,14 @@ const
   max = 50;
 
 type
-  arr_str = array[1..max] of string;
-  arr_int = array[1..max] of integer;
-  arr_char = array[1..max] of char;
+  arr_str = array[0..max] of string;
+  arr_int = array[0..max] of integer;
+  arr_char = array[0..max] of char;
 
 var
   nim, nama: arr_str;
   nilai: arr_int;
-  indeks: arr_char;
-  jml_mhsiswa: integer;
+  i, jml_mhsiswa: integer;
 
 procedure jumlah_data(var jml_mhsiswa: integer);
 { I.S: User memasukkan banyaknya data (jml_mhsiswa) }
@@ -31,43 +30,60 @@ begin
   clrscr;
 end;
 
-function indeks_nilai(nilai: integer): char;
+function idx(nilai: integer): char;
 { I.S.: (nilai) sudah terdefinisi }
 { F.S.: menghasilkan fungsi indeks nilai }
 begin
   case (nilai) of
-    80..100: indeks_nilai := 'A';
-    70..79: indeks_nilai := 'B';
-    60..69: indeks_nilai := 'C';
-    50..59: indeks_nilai := 'D';
-    0..49: indeks_nilai := 'E';
+    80..100: idx := 'A';
+    70..79: idx := 'B';
+    60..69: idx := 'C';
+    50..59: idx := 'D';
+    0..49: idx := 'E';
   end;
 end;
 
 function rata_rata(total: integer): real;
 { I.S.: banyaknya data(nilai) dan total sudah terdefinisi }
 { F.S.: menghasilkan fungsi rata-rata }
-var i: integer;
 begin
   rata_rata := total / jml_mhsiswa;
 end;
 
+function nilai_min(nilai1, nilai2: integer): integer;
+{ I.S.: data(nilai) sudah terdefinisi }
+{ F.S.: menghasilkan fungsi nilai terendah }
+begin
+  nilai_min := nilai1;
+  if nilai2 < nilai_min then
+    nilai_min := nilai2;
+end;
+
+function nilai_max(nilai1, nilai2: integer): integer;
+{ I.S.: data(nilai) sudah terdefinisi }
+{ F.S.: menghasilkan fungsi nilai tertinggi }
+begin
+  nilai_max := nilai1;
+  if nilai2 > nilai_max then
+    nilai_max := nilai2;
+end;
+
 procedure tampil_data;
 var 
-  total, i: integer;
-  // function indeks_nilai(nilai: integer); // prototype 
+  total, n_min, n_max: integer;
+  // function idx(nilai: integer); // prototype 
 begin
   if (jml_mhsiswa > 0) or (jml_mhsiswa <= max) then begin
     window(1, 1, 100, 30);
     clrscr;
     gotoxy(39, 1); // dibagi 2
-    writeln('DAFTAR NILAI MAHASISWA');
+    write('DAFTAR NILAI MAHASISWA');
     gotoxy(7, 2);
-    writeln('-------------------------------------------------------------------------------------');
+    write('-------------------------------------------------------------------------------------');
     gotoxy(7, 3);
-    writeln('| No |      NIM      |      Nama Mahasiswa      |      Nilai      |      Indeks     |');
+    write('| No |      NIM      |      Nama Mahasiswa      |      Nilai      |      Indeks     |');
     gotoxy(7, 4);
-    writeln('-------------------------------------------------------------------------------------');
+    write('-------------------------------------------------------------------------------------');
     
     total := 0;
 
@@ -80,13 +96,16 @@ begin
       gotoxy(91, i + 5); write('|'); // Indeks
       // clreol; gotoxy(1, i + 6); write('---------------------------------------------------------------------------'); clreol;
 
-      gotoxy(13, i + 5); readln(nim[1]);
-      gotoxy(29, i + 5); readln(nama[i]);
-      gotoxy(56, i + 5); readln(nilai[i]);
-      indeks[i] := indeks_nilai(nilai[i]);
-      gotoxy(74, i + 5); write(indeks[i]);
+      gotoxy(13, i + 5); readln(nim[i]); // input NIM
+      gotoxy(29, i + 5); readln(nama[i]); // input Nama
+      gotoxy(56, i + 5); readln(nilai[i]); // input Nilai
+      gotoxy(74, i + 5); write(idx(nilai[i])); // input Indeks
 
       total := total + nilai[i];
+      if i = (jml_mhsiswa - 1) then begin 
+        n_min := nilai_min(nilai[i], nilai[i - 1]);
+        n_max := nilai_max(nilai[i], nilai[i - 1]); 
+      end;
     end;
 
     gotoxy(7, i + 6);
@@ -94,16 +113,15 @@ begin
     gotoxy(7, i + 7);
     writeln('Rata-rata nilai dari ', jml_mhsiswa, ' mahasiswa, yaitu: ', rata_rata(total):0:1);
     // tambahkan fungsi nilai tertinggi dan terendah
+    gotoxy(7, i + 8);
+    writeln('Nilai terendah dari ', jml_mhsiswa, ' mahasiswa, yaitu: ', n_min);
+    gotoxy(7, i + 9);
+    writeln('Nilai tertinggi dari ', jml_mhsiswa, ' mahasiswa, yaitu: ', n_max);
   end else
     writeln('Ga bisa nambahin mahasaiswa, karena penuh');
 end;
 
-{ Kamus Global }
-// procedure tampil_data;
-
 begin
   jumlah_data(jml_mhsiswa);
   tampil_data;
-
-  // tambahkan fungsi nilai tertinggi dan terendah
 end.
