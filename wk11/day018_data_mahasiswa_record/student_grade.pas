9698b9f83a3b84dd0ac63bcbd62854fbfc671886
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2016-12-08 07:11:35 +0700

    [HOMEWORK - Wk11] College Student - Fix excel-like layouting - Fix nilai_min, nilai_max function - Add new presentase function

--- wk11/day018_data_mahasiswa_record/student_grade.pas
+++ wk11/day018_data_mahasiswa_record/student_grade.pas
@@ -6,17 +6,20 @@ const
   max = 50;
 
 type
-  mhs = record
+  mhs = record // record
     nim, nama: string;
     nilai: integer;
     indeks: char;
   end;
   
-  arr_mhs = array[0..max] of mhs;
+  arr_std = array[0..1] of string;
+  arr_mhs = array[0..max] of mhs; // array of record
+  arr_idx = array[0..4] of integer; // array index
 
 var
-  mahasiswa: arr_mhs;
-  i, jml_mhs: integer;
+  mahasiswa: arr_mhs; // mahasiswa
+  idx_mhs: arr_idx; // simpan data tiap indeks
+  i, jml_mhs, idxA, idxB, idxC, idxD, idxE: integer;
 
 procedure jumlah_data(var jml_mhs: integer);
 { I.S: User memasukkan banyaknya data (jml_mhs) }
@@ -35,10 +38,10 @@ end;
 
 function idx(nilai: integer): char;
 { I.S.: (nilai) sudah terdefinisi }
-{ F.S.: menghasilkan fungsi indeks nilai }
+{ F.S.: menghasilkan fungsi idx nilai }
 begin
   case (nilai) of
-    80..100: idx := 'A';
+    80..100: idx := 'A'; // nama variabel / fungsi?
     70..79: idx := 'B';
     60..69: idx := 'C';
     50..59: idx := 'D';
@@ -60,7 +63,7 @@ var i: integer;
 begin
   nilai_min := mahasiswa[0].nilai;
 
-  for i := 1 to (jml_mhs - 1) do begin
+  for i := 1 to (jml_mhs - 1) do begin // 
     if (mahasiswa[i].nilai < nilai_min) then begin
       nilai_min := mahasiswa[i].nilai;
     end;
@@ -81,13 +84,54 @@ begin
   end;
 end;
 
+// Check each index
+procedure jumlah_mhs_tiap_idx(idx: char);
+begin
+  // for i := 0 to 4 do begin
+    case (idx) of
+      'A': idx_mhs[0] := idx_mhs[0] + 1;
+      'B': idx_mhs[1] := idx_mhs[1] + 1;
+      'C': idx_mhs[2] := idx_mhs[2] + 1;
+      'D': idx_mhs[3] := idx_mhs[3] + 1;
+      'E': idx_mhs[4] := idx_mhs[4] + 1;
+    end;
+
+    idxA := idx_mhs[0];
+    idxB := idx_mhs[1];
+    idxC := idx_mhs[2];
+    idxD := idx_mhs[3];
+    idxE := idx_mhs[4];
+  // end;
+end;
+
+// kelulusan + presentasi
+function presentase(req: integer): real;
+var total: integer;
+begin
+  total := 0;
+  case (req) of
+    1: begin
+      for i := 0 to 2 do begin
+        total := total + idx_mhs[i]; 
+      end;
+    end;
+    -1: begin
+      for i := 3 to 4 do begin
+        total := total + idx_mhs[i]; 
+      end;
+    end;
+  end;
+  
+  presentase := (total / jml_mhs) * 100;
+end;
+
 procedure tampil_data;
 var 
   total: integer;
   // function idx(nilai: integer); // prototype 
 begin
   if (jml_mhs > 0) and (jml_mhs <= max) then begin
-    window(1, 1, 100, 30);
+    window(1, 1, 100, 100);
     clrscr;
     gotoxy(39, 1); // dibagi 2
     write('DAFTAR NILAI MAHASISWA');
@@ -99,6 +143,7 @@ begin
     write('-------------------------------------------------------------------------------------');
     
     total := 0;
+    idxA := 0; idxB := 0; idxC := 0; idxD := 0; idxE := 0; 
 
     for i := 0 to (jml_mhs - 1) do begin // array 0
       // gotoxy(x, y)
@@ -112,20 +157,28 @@ begin
       gotoxy(13, i + 5); readln(mahasiswa[i].nim); // input NIM
       gotoxy(29, i + 5); readln(mahasiswa[i].nama); // input Nama
       gotoxy(56, i + 5); readln(mahasiswa[i].nilai); // input Nilai
-      gotoxy(74, i + 5); write(idx(mahasiswa[i].nilai)); // input Indeks
+      mahasiswa[i].indeks := idx(mahasiswa[i].nilai);
+      gotoxy(74, i + 5); write(mahasiswa[i].indeks); // input Indeks
 
       total := total + mahasiswa[i].nilai;
+      jumlah_mhs_tiap_idx(mahasiswa[i].indeks); // increment each index
     end;
 
     gotoxy(7, i + 6);
     write('-------------------------------------------------------------------------------------');
-    gotoxy(7, i + 7);
-    write('Rata-rata nilai dari ', jml_mhs, ' mahasiswa, yaitu: ', rata_rata(total):0:1);
+    gotoxy(7, i + 7); write('Rata-rata nilai dari ', jml_mhs, ' mahasiswa, yaitu: ', rata_rata(total):0:1);
     // tambahkan fungsi nilai tertinggi dan terendah
-    gotoxy(7, i + 8);
-    write('Nilai terendah dari ', jml_mhs, ' mahasiswa, yaitu: ', nilai_min(mahasiswa));
-    gotoxy(7, i + 9);
-    writeln('Nilai tertinggi dari ', jml_mhs, ' mahasiswa, yaitu: ', nilai_max(mahasiswa));
+    gotoxy(7, i + 8); write('Nilai terendah dari ', jml_mhs, ' mahasiswa, yaitu: ', nilai_min(mahasiswa));
+    gotoxy(7, i + 9); write('Nilai tertinggi dari ', jml_mhs, ' mahasiswa, yaitu: ', nilai_max(mahasiswa));
+    // jumlah mahasiswa per-indeks
+    gotoxy(7, i + 10); write('Mahasiswa dengan indeks A, yaitu: ', idxA);
+    gotoxy(7, i + 11); write('Mahasiswa dengan indeks B, yaitu: ', idxB);
+    gotoxy(7, i + 12); write('Mahasiswa dengan indeks C, yaitu: ', idxC);
+    gotoxy(7, i + 13); write('Mahasiswa dengan indeks D, yaitu: ', idxD);
+    gotoxy(7, i + 14); write('Mahasiswa dengan indeks E, yaitu: ', idxE);
+    // presentase kelulusan
+    gotoxy(7, i + 15); write('Mahasiswa yang lulus: ', presentase(1):0:1, '%');
+    gotoxy(7, i + 14); write('Mahasiswa yang tidak lulus: ', presentase(-1):0:1, '%');
   end else
     writeln('Ga bisa nambahin mahasaiswa, karena penuh');
 end;
@@ -133,4 +186,5 @@ end;
 begin
   jumlah_data(jml_mhs);
   tampil_data;
+  readln;
 end.
