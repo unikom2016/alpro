819c3a3933d06a1fcf21052342bf4e6bad2de530
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2016-12-22 16:58:18 +0700

    [HOMEWORK - Wk13] Library Data Input - add new field for sorting (kode, nama, tahun, pengarang)

--- wk13/day22_lab/sorting_practice.pas
+++ wk13/day22_lab/sorting_practice.pas
@@ -64,6 +64,8 @@ end;
 
 // prosedur memasukkan data buku
 procedure isi_data_buku(var n: integer; var bk: arr_buku);
+{ I.S.: user memasukkan banyaknya data (n) dan data buku }
+{ F.S.: menghasilkan banyaknya data (n) dan data buku }
 var i: integer;
 begin
   clrscr;
@@ -81,6 +83,7 @@ begin
     gotoxy(60, i + 12); readln(bk[i].tahun);
     gotoxy(68, i + 12); readln(bk[i].pengarang);
   end;
+  gotoxy(30, i + 13);   write('==================================================');
 end;
 
 procedure swap(var a, b: dbuku);
@@ -92,19 +95,107 @@ begin
   b := c;
 end;
 
+// sortir sesuai kode buku, secara ascending
+// procedure sort_asc(var bk: arr_buku; kd_buku1, kd_buku2: integer);
+// begin
+//   if (kd_buku1 > kd_buku2) then begin
+//     swap(bk)
+//   end;
+// end;
+
+function urut_berdasarkan: integer;
+var
+  pilih: integer;
+begin
+  clrscr;
+  gotoxy(1, 1); write('Urutkan berdasarkan');
+  gotoxy(1, 2); write('1. Kode buku');
+  gotoxy(1, 3); write('2. Nama buku');
+  gotoxy(1, 4); write('3. Tahun');
+  gotoxy(1, 5); write('4. Pengarang');
+  gotoxy(1, 6); write('Pilihan anda? '); readln(pilih);
+  while (pilih < 1) and (pilih > 4) do begin
+    gotoxy(1, 7); write('Pilihan hanya 1-4!');
+    readln;
+    gotoxy(1, 7); clreol; // hapus error
+    gotoxy(14, 6); clreol; readln(pilih);
+  end;
+  urut_berdasarkan := pilih;
+end;
+
 // prosedur mengurutkan data buku
 procedure urut_data_buku(n: integer; var bk: arr_buku);
 var
-  i: integer;
+  urut, i: integer;
   swapped: boolean;
+  metode: string;
 begin
+  clrscr;
+  // write('Menggunakan (bubble / selection): '); readln(metode);
+  urut := urut_berdasarkan;
+  write('Menggunakan urutan (asc / desc): '); readln(metode);
   repeat
     swapped := false;
     for i := 1 to (n - 1) do begin
-      // ascending
-      if (bk[i].kd_buku > bk[i + 1].kd_buku) then begin
-        swap(bk[i], bk[i + 1]);
-        swapped := true;
+      case (metode) of
+        // ascending
+        'asc': begin
+          case (urut) of
+            1: begin // sortir sesuai kode buku
+              if (bk[i].kd_buku > bk[i + 1].kd_buku) then begin
+                swap(bk[i], bk[i + 1]);
+                swapped := true;
+              end;
+            end;
+            2: begin // sortir sesuai nama buku
+              if (bk[i].nm_buku > bk[i + 1].nm_buku) then begin
+                swap(bk[i], bk[i + 1]);
+                swapped := true;
+              end;
+            end;
+            3: begin // sortir sesuai tahun
+              if (bk[i].tahun > bk[i + 1].tahun) then begin
+                swap(bk[i], bk[i + 1]);
+                swapped := true;
+              end;
+            end;
+            4: begin // sortir sesuai pengarang
+              if (bk[i].pengarang > bk[i + 1].pengarang) then begin
+                swap(bk[i], bk[i + 1]);
+                swapped := true;
+              end;
+            end;
+          end; // end urut field
+        end; // end ascending
+        // descending
+        'desc': begin
+          case (urut) of
+            1: begin // sortir sesuai kode buku
+              if (bk[i].kd_buku < bk[i + 1].kd_buku) then begin
+                swap(bk[i], bk[i + 1]);
+                swapped := true;
+              end;
+            end;
+            2: begin // sortir sesuai nama buku
+              if (bk[i].nm_buku < bk[i + 1].nm_buku) then begin
+                swap(bk[i], bk[i + 1]);
+                swapped := true;
+              end;
+            end;
+            3: begin // sortir sesuai tahun
+              if (bk[i].tahun < bk[i + 1].tahun) then begin
+                swap(bk[i], bk[i + 1]);
+                swapped := true;
+              end;
+            end;
+            4: begin // sortir sesuai pengarang
+              if (bk[i].pengarang < bk[i + 1].pengarang) then begin
+                swap(bk[i], bk[i + 1]);
+                swapped := true;
+              end;
+            end;
+          end; // end urut field
+        end; // end descending
       end;
     end;
   until (not swapped);
@@ -112,8 +203,22 @@ end;
 
 // prosedur menampilkan data buku yang sudah terurut
 procedure tampil_data_buku(n: integer; bk: arr_buku);
+var i: integer;
 begin
-  
+  clrscr;
+  gotoxy(30, 9);        write('DAFTAR BUKU');
+  gotoxy(30, 10);       write('==================================================');
+  gotoxy(30, 11);       write('| No | Kode Buku | Nama Buku | Tahun | Pengarang |');
+  gotoxy(30, 12);       write('==================================================');
+  for i := 1 to n do begin
+    gotoxy(30, i + 12); write('|    |           |           |       |           |');
+    gotoxy(32, i + 12); write(i);
+    gotoxy(36, i + 12); write(bk[i].kd_buku);
+    gotoxy(48, i + 12); write(bk[i].nm_buku);
+    gotoxy(60, i + 12); write(bk[i].tahun);
+    gotoxy(68, i + 12); write(bk[i].pengarang);
+  end;
+  gotoxy(30, i + 13);   write('==================================================');
 end;
 
 { Algoritma Utama }
